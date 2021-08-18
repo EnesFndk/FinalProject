d50cd63a1b8ac4b360b422a2233557e3c51a1017
Enes Fındık <enesfindik.34@gmail.com>
2021-08-19 01:07:01 +0300

    11

---
+++ WebAPIx/Controllers/ProductsController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Absract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //[ApiController] bu bir Attribute'dür . Attribute nedir ? Attribute ise bir class ile ilgili bilgi verme yöntemidir.
+        //API'lerde çoğul isim veriyoruz.Product değil Products, Category değil Categories.
+        //IoC container - Inversion of Control = yani bu bir havuzda newlenmiş olan referanslar var onları istediğimizde alabiliyoruz onun içinden. WEBAPI'da Startup içerisine yazdık. "services.AddSingleton<IProductsService,ProductManager>();"
+        IProductsService _productsService;
+
+        public ProductsController(IProductsService productsService)
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Dependency Chain var burda = yani IProductsService ProductManager'e ihtiyaç duyuyor. ProductManager ise EfProductDal'a ihtiyaç  duyuyor.
+            //Kötü kod = IProductsService productsService = new ProductManager(new EfProductDal());
+            //yukarıdaki kötü kod yerine startup'da IoC ile Newlenebilir yapılar yaptık ve clean cod olarak yazdık.
+            //Postman'deki Status önemli oraya bakarak veri gelişini görebiliyoruz. Baktık 400 bad request yaptı = sistem bakımda mesajı verdi. status 200 OK ise çalışıyor.
+
+            var result = _productsService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        //Get içine int id koyduk çünkü üstteki ile aynı olduğu için çalışmaz ve GetById için.
+        //isim veriyoruz ki karışmasın çünkü çalıştırınca patlıyor ("getall") yada ("getbyid")
+        //postman'de çalışması için ise https://localhost:44368/api/products/getall sonuna getall yada getbyid yazıyoruz.
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productsService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        //Bu httpPost postman'de get yerine post'u seçiyoruz. Bu işlem Dataya veri göndericez.
+        //Postman'de Post seçtikten sonra Body'yi , raw'ı ve Json'ı seçip veriyi id'siz şekilde ekliyoruz.
+        //sağlama yapmak için Get'e gelip Send yaparsak en aşağıda girdiğimiz veri gözükecektir.
+        //silme (delete) ve güncelleme (update) içinde httpPost kullanabiliriz.
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productsService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
---
+++ WebAPIx/Startup.cs
@@ -0,0 +1,62 @@
+using Business.Absract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIx
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            //Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container alt yapýsý kuruyor ve bunlardan birini kullanýcaz. "services.AddSingleton<IProductDal, EfProductDal>();" bunun gibileri yazmamak için. gene clean cod yapýyoruz.
+            //yukarýdaki iþlemler AOP tekniðiyle yapýcaz.
+            //Eðer IProductsService isterse ona newlenmiþ ProductManager ver demek. IoC yöntemi ile.
+
+            services.AddSingleton<IProductsService,ProductManager>();
+            //aþaðýdakinde de IProductDal isterse ona newlenmiþ EfProductDal veriyor. IoC yöntemi ile.
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
